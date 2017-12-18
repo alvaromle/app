5ff9bffb21d84cd8c888a1438901c364e3ed4ab7
Alvaro <alvaro@Alvaros-MacBook-Pro.local>
2017-12-18 22:24:04 +0100

    Login Screen head text changed

--- app-ui/src/main/java/com/gac/app/ui/authentication/LoginScreen.java
+++ app-ui/src/main/java/com/gac/app/ui/authentication/LoginScreen.java
@@ -106,7 +106,7 @@ public class LoginScreen extends CssLayout {
         CssLayout loginInformation = new CssLayout();
         loginInformation.setStyleName("login-information");
         Label loginInfoText = new Label(
-                "<h1>Login Information</h1>"
+                "<h1>Gestión Atención Clientes</h1>"
                         + "Log in as &quot;admin&quot; to have full access. Log in with any other username to have read-only access. For all users, any password is fine",
                 ContentMode.HTML);
         loginInfoText.setSizeFull();
